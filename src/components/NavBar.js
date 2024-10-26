840f41ce94f394755fabff282e7fd0155860c37d
Asya Haroyan <alharoyan@gmail.com>
2024-10-26 19:30:36 +0000

    Add NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+
+const NavBar = () => {
+    return <Navbar expand="md" fixed="top">
+    <Container>
+    <Navbar.Brand>Inspira Logo</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto text-right">
+      <Nav.Link>Post <i class="fa-solid fa-arrow-up-from-bracket"></i></Nav.Link>
+        <Nav.Link>Home <i class="fa-solid fa-archway"></i></Nav.Link>
+        <Nav.Link>Sign In <i class="fa-solid fa-arrow-right-to-bracket"></i></Nav.Link>
+        <Nav.Link>Sign Up <i class="fa-solid fa-user-plus"></i></Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+    </Container>
+  </Navbar>
+};
+
+export default NavBar;
