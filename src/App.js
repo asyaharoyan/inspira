f406a8e163d7d04fa9fd7bc8b5c75d0a21a630e5
Asya Haroyan <alharoyan@gmail.com>
2024-10-29 08:29:59 +0000

    Add sign up form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Container } from 'react-bootstrap';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Route, Switch } from 'react-router-dom';
+import "./api/axiosDefaults";
+import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
 
 function App() {
   return (
@@ -11,8 +14,8 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
           <Route exact path="/post" render={() => <h1>Post Page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <h1>Page not found!</h1>} />
         </Switch>
       </Container>
---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://inspira-api-bc9117602418.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
