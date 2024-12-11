6f7f5e81d2fffab00e54f4aacc9d1346c12eef61
Asya Haroyan <alharoyan@gmail.com>
2024-12-11 08:36:54 +0000

    Add popular profiles which has the most posts

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
-// import Asset from "../../components/Asset";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Asset from "../../components/Asset";
 // import { useProfileData } from "../../contexts/ProfileDataContext";
-// import Profile from "./Profile";
+import Profile from "./Profile";
 
 const PopularProfiles = ({ mobile }) => {
-//   const { popularProfiles } = useProfileData();
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser()
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-post_count"
+        );
+        const filteredProfiles = data.results.filter(
+          (profile) => profile.id !== currentUser?.profile_id
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: { results: filteredProfiles },
+        }));
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
 
   return (
     <Container
@@ -14,7 +42,24 @@ const PopularProfiles = ({ mobile }) => {
         mobile && "d-lg-none text-center mb-3"
       }`}
     >
-      Popular profiles
+      {popularProfiles.results.length ? (
+        <>
+        {mobile ? (
+          <div className="d-flex justify-content-around">
+            {popularProfiles.results.slice(0, 4).map((profile) => (
+              <Profile key={profile.id} profile={profile} mobile />
+      ))}
+          </div>
+        ) : (
+          popularProfiles.results.map((profile) => (
+            <Profile key={profile.id} profile={profile} />
+          ))
+        )}
+      </>
+      ) : (
+        <Asset spinner />
+      )}
+      
     </Container>
   );
 };
